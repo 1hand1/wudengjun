de7cb4eacf8fda3aadef5c84fab3c9fccfa3afd6
sink <33800637+1hand1@users.noreply.github.com>
2022-06-23 10:13:00 +0800

    Create MimeTypeUtils.java

---
+++ MimeTypeUtils.java
@@ -0,0 +1,16 @@
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.UnsupportedCharsetException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+/**
+ * Comparator used by {@link #sortBySpecificity(List)}.
+ *
+ **/
+public abstract class MimeTypeUtils {
+					'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                            'V', 'W', 'X', 'Y', 'Z'
+    private static final Random RND = new Random();
+    private static final Random RND = new SecureRandom();
+}
